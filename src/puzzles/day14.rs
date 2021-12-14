770d41f4e17c68e7fe0686a91cf12e100c732525
Romain Delamare <romain@delama.re>
2021-12-14 13:55:03 +0100

    add day 14 part 2

--- src/puzzles/day14.rs
+++ src/puzzles/day14.rs
@@ -20,22 +20,27 @@ impl Puzzle for Day14 {
     }
 
     fn part_one(&self) -> String {
-        let result = POLYMERIZATION.apply_n_times(TEMPLATE, 10);
-        let count = PolymerCount::new(&result).unwrap();
+        let min_max = POLYMERIZATION.apply(TEMPLATE, 10);
         format!(
-            "Difference between most present element ({}) and least present element ({}): {}",
-            count.max_char,
-            count.min_char,
-            count.max_count - count.min_count
+            "Difference between most present element ({}) and least present element ({}) after 10 iterations: {}",
+            min_max.max_element,
+            min_max.min_element,
+            min_max.max_count - min_max.min_count
         )
     }
 
     fn part_two(&self) -> String {
-        "".to_string()
+        let min_max = POLYMERIZATION.apply(TEMPLATE, 40);
+        format!(
+            "Difference between most present element ({}) and least present element ({}) after 40 iterations: {}",
+            min_max.max_element,
+            min_max.min_element,
+            min_max.max_count - min_max.min_count
+        )
     }
 }
 
-struct PolymerCount {
+/*struct PolymerCount {
     min_char: char,
     min_count: usize,
     max_char: char,
@@ -59,28 +64,28 @@ impl PolymerCount {
             _ => Err(()),
         }
     }
-}
+}*/
 
 struct Polymerization(HashMap<(char, char), char>);
 
 impl Polymerization {
-    fn apply(&self, template: &str) -> String {
-        let mut result: String = template
-            .chars()
-            .tuple_windows()
-            .flat_map(|pair| match self.0.get(&pair) {
-                Some(c) => vec![pair.0, *c],
-                None => vec![pair.0],
-            })
-            .collect();
-        if let Some(last) = template.chars().last() {
-            result.push(last)
-        }
-        result
-    }
+    fn apply(&self, template: &str, n: usize) -> ElementMinMax {
+        let mut pair_count = PairCount::from(template);
 
-    fn apply_n_times(&self, template: &str, n: usize) -> String {
-        (0..n).fold(template.to_string(), |s, _| self.apply(&s))
+        for _ in 0..n {
+            let mut new_pair_count = PairCount::new();
+            for ((c1, c2), count) in pair_count {
+                if let Some(c) = self.0.get(&(c1, c2)) {
+                    new_pair_count.add_pair(c1, *c, count);
+                    new_pair_count.add_pair(*c, c2, count);
+                } else {
+                    new_pair_count.add_pair(c1, c2, count);
+                }
+            }
+            pair_count = new_pair_count;
+        }
+
+        pair_count.count_elements(template.chars().next()).unwrap()
     }
 }
 
@@ -98,6 +103,65 @@ impl<T: IntoIterator<Item = InsertionRule>> From<T> for Polymerization {
                 .collect(),
         )
     }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct PairCount(HashMap<(char, char), usize>);
+
+impl PairCount {
+    fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    fn from(template: &str) -> Self {
+        Self(template.chars().tuple_windows().counts())
+    }
+
+    fn add_pair(&mut self, c1: char, c2: char, count: usize) {
+        let current_count = self.0.entry((c1, c2)).or_insert(0);
+        *current_count += count;
+    }
+
+    fn count_elements(&self, first_element: Option<char>) -> Result<ElementMinMax, ()> {
+        let mut all_count = first_element
+            .map(|c| [(c, 1)].into())
+            .unwrap_or_else(HashMap::new);
+
+        for ((_, c), n) in &self.0 {
+            let count = all_count.entry(*c).or_insert(0);
+            *count += n;
+        }
+
+        let minmax = all_count
+            .into_iter()
+            .minmax_by(|(_, n1), (_, n2)| n1.cmp(n2));
+        match minmax {
+            MinMaxResult::MinMax(min, max) => Ok(ElementMinMax {
+                min_element: min.0,
+                min_count: min.1,
+                max_element: max.0,
+                max_count: max.1,
+            }),
+            _ => Err(()),
+        }
+    }
+}
+
+impl IntoIterator for PairCount {
+    type Item = ((char, char), usize);
+    type IntoIter = std::collections::hash_map::IntoIter<(char, char), usize>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct ElementMinMax {
+    min_element: char,
+    min_count: usize,
+    max_element: char,
+    max_count: usize,
 }
 
 struct InsertionRule {
@@ -148,21 +212,17 @@ CN -> C
     }
 
     #[test]
-    fn sample_polymerization_should_return_ncnbchb_when_applied_to_nncb_once() {
-        assert_eq!(&SAMPLE.apply("NNCB"), "NCNBCHB");
-    }
+    fn test_sample_after_10_iterations() {
+        let min_max = SAMPLE.apply("NNCB", 10);
 
-    #[test]
-    fn sample_polymerization_should_return_nbccnbbbcbhcb_when_applied_to_nncb_twice() {
-        assert_eq!(&SAMPLE.apply_n_times("NNCB", 2), "NBCCNBBBCBHCB");
-    }
-
-    #[test]
-    fn sample_polymerization_should_return_nbbbcnccnbbnbnbbchbhhbchbb_when_applied_to_nncb_thrice()
-    {
         assert_eq!(
-            &SAMPLE.apply_n_times("NNCB", 3),
-            "NBBBCNCCNBBNBNBBCHBHHBCHB"
-        );
+            min_max,
+            ElementMinMax {
+                min_element: 'H',
+                min_count: 161,
+                max_element: 'B',
+                max_count: 1749,
+            }
+        )
     }
 }
