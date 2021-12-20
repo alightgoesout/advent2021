8c09675374afeb91bf5c2df66d658500526d783f
Romain Delamare <romain@delama.re>
2021-12-20 18:11:58 +0100

    refactor day 16

--- src/puzzles/day16.rs
+++ src/puzzles/day16.rs
@@ -17,7 +17,7 @@ impl Puzzle for Day16 {
     fn part_one(&self) -> String {
         let mut parser = PacketParser::new(&INPUT);
         let packet = parser.parse().unwrap();
-        format!("Sum of all version: {}", packet.versions_sum())
+        format!("Sum of all version: {}", packet.sum_versions())
     }
 
     fn part_two(&self) -> String {
@@ -30,75 +30,80 @@ impl Puzzle for Day16 {
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct Packet {
     version: u8,
-    content: PacketContent,
+    kind: PacketKind,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum PacketKind {
+    Literal(u64),
+    Sum(Vec<Packet>),
+    Product(Vec<Packet>),
+    Minimum(Vec<Packet>),
+    Maximum(Vec<Packet>),
+    GreaterThan {
+        first: Box<Packet>,
+        second: Box<Packet>,
+    },
+    LessThan {
+        first: Box<Packet>,
+        second: Box<Packet>,
+    },
+    Equal {
+        first: Box<Packet>,
+        second: Box<Packet>,
+    },
 }
 
 impl Packet {
-    fn versions_sum(&self) -> u32 {
-        match &self.content {
-            PacketContent::Literal(_) => self.version as u32,
-            PacketContent::Operator { sub_packets, .. } => {
-                (self.version as u32) + sub_packets.iter().map(Packet::versions_sum).sum::<u32>()
+    fn sum_versions(&self) -> u32 {
+        match &self.kind {
+            PacketKind::Literal(_) => self.version as u32,
+            PacketKind::Sum(sub_packets)
+            | PacketKind::Product(sub_packets)
+            | PacketKind::Minimum(sub_packets)
+            | PacketKind::Maximum(sub_packets) => {
+                self.version as u32 + sub_packets.iter().map(Packet::sum_versions).sum::<u32>()
+            }
+            PacketKind::GreaterThan { first, second }
+            | PacketKind::LessThan { first, second }
+            | PacketKind::Equal { first, second } => {
+                self.version as u32 + first.sum_versions() + second.sum_versions()
             }
         }
     }
 
     fn evaluate(&self) -> u64 {
-        match &self.content {
-            PacketContent::Literal(v) => *v,
-            PacketContent::Operator {
-                operation,
-                sub_packets,
-            } => operation.apply(sub_packets),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-enum PacketContent {
-    Literal(u64),
-    Operator {
-        operation: Operation,
-        sub_packets: Vec<Packet>,
-    },
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-enum Operation {
-    Sum,
-    Product,
-    Minimum,
-    Maximum,
-    GreaterThan,
-    LessThan,
-    Equal,
-}
-
-impl Operation {
-    fn apply(&self, parameters: &[Packet]) -> u64 {
-        match self {
-            Operation::Sum => parameters.iter().map(Packet::evaluate).sum(),
-            Operation::Product => parameters.iter().map(Packet::evaluate).product(),
-            Operation::Minimum => parameters.iter().map(Packet::evaluate).min().unwrap(),
-            Operation::Maximum => parameters.iter().map(Packet::evaluate).max().unwrap(),
-            Operation::GreaterThan => (parameters[0].evaluate() > parameters[1].evaluate()) as u64,
-            Operation::LessThan => (parameters[0].evaluate() < parameters[1].evaluate()) as u64,
-            Operation::Equal => (parameters[0].evaluate() == parameters[1].evaluate()) as u64,
-        }
-    }
-}
-
-impl Operation {
-    fn from_id(operator_id: u8) -> Self {
-        match operator_id {
-            0 => Self::Sum,
-            1 => Self::Product,
-            2 => Self::Minimum,
-            3 => Self::Maximum,
-            5 => Self::GreaterThan,
-            6 => Self::LessThan,
-            7 => Self::Equal,
-            _ => panic!("Unknown operator id: {}", operator_id),
+        match &self.kind {
+            PacketKind::Literal(value) => *value,
+            PacketKind::Sum(sub_packets) => sub_packets.iter().map(Packet::evaluate).sum(),
+            PacketKind::Product(sub_packets) => sub_packets.iter().map(Packet::evaluate).product(),
+            PacketKind::Minimum(sub_packets) => {
+                sub_packets.iter().map(Packet::evaluate).min().unwrap_or(0)
+            }
+            PacketKind::Maximum(sub_packets) => {
+                sub_packets.iter().map(Packet::evaluate).max().unwrap_or(0)
+            }
+            PacketKind::GreaterThan { first, second } => {
+                if first.evaluate() > second.evaluate() {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketKind::LessThan { first, second } => {
+                if first.evaluate() < second.evaluate() {
+                    1
+                } else {
+                    0
+                }
+            }
+            PacketKind::Equal { first, second } => {
+                if first.evaluate() == second.evaluate() {
+                    1
+                } else {
+                    0
+                }
+            }
         }
     }
 }
@@ -115,76 +120,96 @@ impl<'a> PacketParser<'a> {
     }
 
     pub fn parse(&mut self) -> Option<Packet> {
-        self.read_packet().map(|(packet, _)| packet)
+        self.parse_packet().map(|(packet, _)| packet)
     }
 
-    fn read_packet(&mut self) -> Option<(Packet, u32)> {
+    fn parse_packet(&mut self) -> Option<(Packet, usize)> {
         let version = self.lexer.read_number(3)? as u8;
+        let type_id = self.lexer.read_number(3)? as u8;
 
-        let (content, content_size) = match self.lexer.read_number(3)? {
+        let (kind, size) = match type_id {
             4 => self.parse_literal()?,
-            operator_id => self.parse_operator(operator_id as u8)?,
+            _ => self.parse_operator(type_id)?,
         };
 
-        Some((Packet { version, content }, 6 + content_size))
+        Some((Packet { version, kind }, 6 + size))
     }
 
-    fn parse_literal(&mut self) -> Option<(PacketContent, u32)> {
+    fn parse_literal(&mut self) -> Option<(PacketKind, usize)> {
         let (value, size) = self.lexer.read_grouped_number()?;
-        Some((PacketContent::Literal(value as u64), size))
+        Some((PacketKind::Literal(value as u64), size))
     }
 
-    fn parse_operator(&mut self, operator_id: u8) -> Option<(PacketContent, u32)> {
-        let operation = Operation::from_id(operator_id);
-        let length_type = self.lexer.next_bit()?;
+    fn parse_operator(&mut self, operator_id: u8) -> Option<(PacketKind, usize)> {
+        let (mut sub_packets, size) = self.parse_sub_packets()?;
+        match operator_id {
+            0 => Some((PacketKind::Sum(sub_packets), size)),
+            1 => Some((PacketKind::Product(sub_packets), size)),
+            2 => Some((PacketKind::Minimum(sub_packets), size)),
+            3 => Some((PacketKind::Maximum(sub_packets), size)),
+            5 => Some((
+                PacketKind::GreaterThan {
+                    first: Box::new(sub_packets.remove(0)),
+                    second: Box::new(sub_packets.remove(0)),
+                },
+                size,
+            )),
+            6 => Some((
+                PacketKind::LessThan {
+                    first: Box::new(sub_packets.remove(0)),
+                    second: Box::new(sub_packets.remove(0)),
+                },
+                size,
+            )),
+            7 => Some((
+                PacketKind::Equal {
+                    first: Box::new(sub_packets.remove(0)),
+                    second: Box::new(sub_packets.remove(0)),
+                },
+                size,
+            )),
+            _ => None,
+        }
+    }
+
+    fn parse_sub_packets(&mut self) -> Option<(Vec<Packet>, usize)> {
+        let length_type = self.lexer.read_number(1)?;
         match length_type {
-            0 => self.parse_type_0_operator(operation),
-            1 => self.parse_type_1_operator(operation),
+            0 => self.parse_type_0_sub_packets(),
+            1 => self.parse_type_1_sub_packets(),
             _ => None,
         }
         .map(|(content, size)| (content, size + 1))
     }
 
-    fn parse_type_0_operator(&mut self, operation: Operation) -> Option<(PacketContent, u32)> {
-        let sub_packets_length = self.lexer.read_number(15)?;
+    fn parse_type_0_sub_packets(&mut self) -> Option<(Vec<Packet>, usize)> {
+        let sub_packets_length = self.lexer.read_number(15)? as usize;
         let mut sub_packets = Vec::new();
         let mut current_length = 0;
         while current_length < sub_packets_length {
-            let (packet, size) = self.read_packet()?;
+            let (packet, size) = self.parse_packet()?;
             sub_packets.push(packet);
-            current_length += size as u64;
+            current_length += size;
         }
-        Some((
-            PacketContent::Operator {
-                operation,
-                sub_packets,
-            },
-            15 + sub_packets_length as u32,
-        ))
+        Some((sub_packets, 15 + sub_packets_length))
     }
 
-    fn parse_type_1_operator(&mut self, operation: Operation) -> Option<(PacketContent, u32)> {
+    fn parse_type_1_sub_packets(&mut self) -> Option<(Vec<Packet>, usize)> {
         let nb_packets = self.lexer.read_number(11)?;
         let mut sub_packets = Vec::new();
         let mut sub_packets_length = 0;
         for _ in 0..nb_packets {
-            let (packet, size) = self.read_packet()?;
+            let (packet, size) = self.parse_packet()?;
             sub_packets.push(packet);
             sub_packets_length += size;
         }
-        Some((
-            PacketContent::Operator {
-                operation,
-                sub_packets,
-            },
-            11 + sub_packets_length,
-        ))
+        Some((sub_packets, 11 + sub_packets_length))
     }
 }
 
 struct PacketLexer<'a> {
     source: &'a [u8],
-    next: usize,
+    next_byte: usize,
     buffer: VecDeque<u8>,
 }
 
@@ -192,7 +217,7 @@ impl<'a> PacketLexer<'a> {
     pub fn new(source: &'a str) -> Self {
         Self {
             source: source.as_bytes(),
-            next: 0,
+            next_byte: 0,
             buffer: VecDeque::new(),
         }
     }
@@ -205,7 +230,7 @@ impl<'a> PacketLexer<'a> {
         Some(number)
     }
 
-    pub fn read_grouped_number(&mut self) -> Option<(u64, u32)> {
+    pub fn read_grouped_number(&mut self) -> Option<(u64, usize)> {
         let mut prefix = self.next_bit()?;
         let mut number = self.read_number(4)?;
         let mut total_bits = 5;
@@ -225,18 +250,19 @@ impl<'a> PacketLexer<'a> {
     }
 
     fn fill_buffer(&mut self) {
-        if let Some(n) = self.source.get(self.next) {
-            let mut bits = (*n as char).to_digit(16).unwrap() as u8;
+        for _ in 0..4 {
+            if let Some(n) = self.source.get(self.next_byte) {
+                let bits = (*n as char).to_digit(16).unwrap() as u8;
 
-            let n0 = bits % 2;
-            bits /= 2;
-            let n1 = bits % 2;
-            bits /= 2;
-            let n2 = bits % 2;
-            let n3 = bits / 2;
-            self.buffer.extend([n3, n2, n1, n0]);
+                self.buffer.push_back((bits & 0b1000) >> 3);
+                self.buffer.push_back((bits & 0b0100) >> 2);
+                self.buffer.push_back((bits & 0b0010) >> 1);
+                self.buffer.push_back(bits & 0b0001);
 
-            self.next += 1;
+                self.next_byte += 1;
+            } else {
+                break;
+            }
         }
     }
 }
@@ -255,9 +281,9 @@ mod test {
             result,
             Some(Packet {
                 version: 6,
-                content: PacketContent::Literal(2021),
+                kind: PacketKind::Literal(2021),
             }),
-        )
+        );
     }
 
     #[test]
@@ -270,18 +296,15 @@ mod test {
             result,
             Packet {
                 version: 1,
-                content: PacketContent::Operator {
-                    operation: Operation::LessThan,
-                    sub_packets: vec![
-                        Packet {
-                            version: 6,
-                            content: PacketContent::Literal(10)
-                        },
-                        Packet {
-                            version: 2,
-                            content: PacketContent::Literal(20)
-                        },
-                    ],
+                kind: PacketKind::LessThan {
+                    first: Box::new(Packet {
+                        version: 6,
+                        kind: PacketKind::Literal(10),
+                    }),
+                    second: Box::new(Packet {
+                        version: 2,
+                        kind: PacketKind::Literal(20),
+                    }),
                 },
             },
         )
@@ -297,60 +320,57 @@ mod test {
             result,
             Packet {
                 version: 7,
-                content: PacketContent::Operator {
-                    operation: Operation::Maximum,
-                    sub_packets: vec![
-                        Packet {
-                            version: 2,
-                            content: PacketContent::Literal(1)
-                        },
-                        Packet {
-                            version: 4,
-                            content: PacketContent::Literal(2)
-                        },
-                        Packet {
-                            version: 1,
-                            content: PacketContent::Literal(3)
-                        },
-                    ],
-                },
+                kind: PacketKind::Maximum(vec![
+                    Packet {
+                        version: 2,
+                        kind: PacketKind::Literal(1)
+                    },
+                    Packet {
+                        version: 4,
+                        kind: PacketKind::Literal(2)
+                    },
+                    Packet {
+                        version: 1,
+                        kind: PacketKind::Literal(3)
+                    },
+                ],),
             },
         )
     }
 
     #[test]
-    fn versions_sum_for_8a004a801a8002f478_should_be_16() {
+    fn sum_versions_should_return_16_for_8a004a801a8002f478() {
         let mut parser = PacketParser::new("8A004A801A8002F478");
 
         let packet = parser.parse().unwrap();
 
-        assert_eq!(packet.versions_sum(), 16);
+        assert_eq!(packet.sum_versions(), 16);
     }
 
     #[test]
-    fn versions_sum_for_620080001611562c8802118e34_should_be_12() {
+    fn sum_versions_should_return_12_for_620080001611562c8802118e34() {
         let mut parser = PacketParser::new("620080001611562C8802118E34");
 
         let packet = parser.parse().unwrap();
 
-        assert_eq!(packet.versions_sum(), 12);
+        assert_eq!(packet.sum_versions(), 12);
     }
 
     #[test]
-    fn versions_sum_for_c0015000016115a2e0802f182340_should_be_23() {
+    fn sum_versions_should_return_23_for_c0015000016115a2e0802f182340() {
         let mut parser = PacketParser::new("C0015000016115A2E0802F182340");
 
         let packet = parser.parse().unwrap();
 
-        assert_eq!(packet.versions_sum(), 23);
+        assert_eq!(packet.sum_versions(), 23);
     }
 
     #[test]
-    fn versions_sum_for_a0016c880162017c3686b18a3d4780_should_be_31() {
+    fn sum_versions_should_return_32_for_a0016c880162017c3686b18a3d4780() {
         let mut parser = PacketParser::new("A0016C880162017C3686B18A3D4780");
 
         let packet = parser.parse().unwrap();
 
-        assert_eq!(packet.versions_sum(), 31);
+        assert_eq!(packet.sum_versions(), 31);
     }
 }
